9809dfa83e63103fe01cd1874986d7550ce3173a
sachinb2010 <sachinbhandari548@gmail.com>
2021-11-30 15:08:11 +0530

    Task

---
+++ script.js
@@ -0,0 +1,70 @@
+
+
+function HoverFirst(){
+    document.getElementById("clickWomen").style.visibility="visible";
+  document.getElementById("clickWomen").style.zIndex="999"; 
+
+}
+
+function remFirst(){
+    document.getElementById("clickWomen").style.visibility="hidden";
+}
+
+// function HoverFirst1(){
+//     document.getElementById("clickWomen").style.visibility="visible";
+//   document.getElementById("clickWomen").style.zIndex="999"; 
+
+// }
+
+// function remFirst1(){
+//     document.getElementById("clickWomen").style.visibility="hidden";
+// }
+// function HoverFirst2(){
+//     document.getElementById("clickWomen").style.visibility="visible";
+//   document.getElementById("clickWomen").style.zIndex="999"; 
+
+// }
+
+// function remFirst2(){
+//     document.getElementById("clickWomen").style.visibility="hidden";
+// }
+// function HoverFirst3(){
+//     document.getElementById("clickWomen").style.visibility="visible";
+//   document.getElementById("clickWomen").style.zIndex="999"; 
+
+// }
+
+// function remFirst3(){
+//     document.getElementById("clickWomen").style.visibility="hidden";
+// }
+// function HoverFirst4(){
+//     document.getElementById("clickWomen").style.visibility="visible";
+//   document.getElementById("clickWomen").style.zIndex="999"; 
+
+// }
+
+// function remFirst4(){
+//     document.getElementById("clickWomen").style.visibility="hidden";
+// }
+// function HoverFirst5(){
+//     document.getElementById("clickWomen").style.visibility="visible";
+//   document.getElementById("clickWomen").style.zIndex="999"; 
+
+// }
+
+// function remFirst5(){
+//     document.getElementById("clickWomen").style.visibility="hidden";
+// }
+// function HoverFirst6(){
+//     document.getElementById("clickWomen").style.visibility="visible";
+//   document.getElementById("clickWomen").style.zIndex="999"; 
+
+// }
+
+// function remFirst6(){
+//     document.getElementById("clickWomen").style.visibility="hidden";
+// }
+
+
+
+
